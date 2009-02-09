046b145fe8c75db33fac060ec4390b18d86ee566
Matt Gibb <powermoveguru@googlemail.com>
2009-02-09 16:39:04 +0000

    Added .gitignore file, deleted erroneous Tetgen folder and added program_folders.m

---
+++ config/program_folders.m
@@ -0,0 +1,8 @@
+% This file lists all folders where external programs are installed.
+
+% If some or all programs are kept in the same folder, folder strings can
+% be refactored with program_folder
+program_folder = '/Users/matthewgibb/Documents/DPhil/programs/';
+
+% list of program directories
+CARP_folder = [program_folder 'CARP/'];
